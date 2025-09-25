31f0b489164eaba0cec0df861b07d2e014e535ce
Mosho <moises.leon@chasacademy.se>
2025-09-25 19:38:52 +0200

    Comment Js

--- script.js
+++ script.js
@@ -11,16 +11,20 @@ const btnList = document.getElementsByClassName("btnInput")
 const showIndisplay = document.getElementById("Display")
 const result = document.getElementById("resultBtn")
 const back = document.getElementById("back")
-const dateStamp = document.getElementById("dateStamp")
+//const dateStamp = document.getElementById("dateStamp")
 
+//Går igenom alla button i btnList och väljer ut en onClick, ,  och sedan om det går åt skogen skriver den ut "ops!" 
 for(button of btnList) {
     const btn = button 
     button.onclick = () => {
         if(showIndisplay.textContent === "0"){
+            //ser till att nollställa räknaren innan du skriver något om det är 0
             showIndisplay.textContent = ""
         }else if(showIndisplay.textContent === "-- 2-long --"){
+            //Nollställer om du har texten -- 2-long --
            showIndisplay.textContent = ""
         }else if(showIndisplay.textContent === "Ops!"){
+            //Nollställer om du har texten Ops!
            showIndisplay.textContent = ""
         } 
         showIndisplay.textContent += btn.textContent
@@ -39,6 +43,7 @@ result.addEventListener("click", () => {
     try {
         const ekvResult = eval(toCalculate)
         showIndisplay.textContent = ekvResult
+        //Validerar om du har för lång resultat längre än 10 tecken
         if (ekvResult.toString().length >= 10) {
             showIndisplay.textContent = "-- 2-long --"
         }
@@ -47,7 +52,7 @@ result.addEventListener("click", () => {
     }
 })
 
-//Slisar den sista siffran
+//Slisar/tarbort den sista siffran
 back.addEventListener("click", () => {
     showIndisplay.textContent = showIndisplay.textContent.slice(0, -1)
 })
@@ -57,8 +62,7 @@ back.addEventListener("click", () => {
 dateStamp.textContent = showDateStamp.toLocaleString("sv-SE") */
 
 
-//Funktion som hämtar sista push av denna repo
-
+//Funktion som hämtar sista push av denna repo källa: ChatPGT
 async function getLastPush() {
     const response = await fetch("https://api.github.com/repos/moooshooo/calculator");
     const data = await response.json();
