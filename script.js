2ce1a6f903c4bd862c1d5aab5be587fc8ee87822
Mosho <moises.leon@chasacademy.se>
2025-09-25 15:43:48 +0200

    Dagens kod

--- script.js
+++ script.js
@@ -4,36 +4,29 @@ Date: 2025-09-25
 Version: 0.1
 Project Name: Miniräknare
 */
-
 console.log("Empowered by: MoS")
 console.log("=================")
 
-//Alla nummer
-const nr1 = document.getElementById("1")
-/* nr1.addEventListener("click", () => {
-    return 1
-}) */
-console.log(nr1)
-const nr2 = document.getElementById("2")
-const nr3 = document.getElementById("3")
-const nr4 = document.getElementById("4")
-const nr5 = document.getElementById("5")
-const nr6 = document.getElementById("6")
-const nr7 = document.getElementById("7")
-const nr8 = document.getElementById("8")
-const nr9 = document.getElementById("9")
+const btnList = document.getElementsByClassName("btnInput")
+const showIndisplay = document.getElementById("Display")
 
-//Matesymboler
-const plus = document.getElementById("plus") 
-const minus = document.getElementById("minus") 
-const multiplication = document.getElementById("multiplication") 
-const divided = document.getElementById("divided") 
 
-const getNumbers = () => {
-    const pushNumber = nr1
+for(button of btnList) {
+    const btn = button 
+    button.onclick = () => {
+        if(showIndisplay.textContent === "0"){
+            showIndisplay.textContent = ""
+        } 
+        showIndisplay.textContent += btn.textContent
+    }
 }
 
-const display = () => {
-    
-
+const allClear = document.getElementById("ac")
+allClear.onclick = () =>{
+    showIndisplay.textContent = 0
 }
+
+const calculatorresult = document.getElementById("resultbtn")
+    result.onclick = () => {
+        const ekvation = btn.textContent += btn.textContent
+    }
