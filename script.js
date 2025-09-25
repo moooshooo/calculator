998025eca5aff386f225a0a4d0a09b8e0f162913
Mosho <moises.leon@chasacademy.se>
2025-09-25 12:03:25 +0200

    rast

---
+++ script.js
@@ -0,0 +1,39 @@
+/* 
+Author: Mos
+Date: 2025-09-25
+Version: 0.1
+Project Name: Miniräknare
+*/
+
+console.log("Empowered by: MoS")
+console.log("=================")
+
+//Alla nummer
+const nr1 = document.getElementById("1")
+/* nr1.addEventListener("click", () => {
+    return 1
+}) */
+console.log(nr1)
+const nr2 = document.getElementById("2")
+const nr3 = document.getElementById("3")
+const nr4 = document.getElementById("4")
+const nr5 = document.getElementById("5")
+const nr6 = document.getElementById("6")
+const nr7 = document.getElementById("7")
+const nr8 = document.getElementById("8")
+const nr9 = document.getElementById("9")
+
+//Matesymboler
+const plus = document.getElementById("plus") 
+const minus = document.getElementById("minus") 
+const multiplication = document.getElementById("multiplication") 
+const divided = document.getElementById("divided") 
+
+const getNumbers = () => {
+    const pushNumber = nr1
+}
+
+const display = () => {
+    
+
+}
